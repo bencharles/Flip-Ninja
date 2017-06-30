28df38193f7a454777333ce4f0a2d6325f3941d1
Ben Charles <benkcharles@gmail.com>
2017-06-30 15:15:00 -0400

    Add moving ground to scene

---
+++ Flip Ninja/BCCharacter.swift
@@ -0,0 +1,19 @@
+//
+//  MLCharacter.swift
+//  Flip Ninja
+//
+//  Created by Ben  Charles on 6/30/17.
+//  Copyright © 2017 Ben  Charles. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class BCCharacter: SKSpriteNode {
+    
+    override init() {
+        super.init(texture: nil, color: UIColor.clear, size: )
+        
+    }
+    
+}
